9d88de218ec34ad980b8220ad736f5e45510ae27
dn210191kos <kosova.olgasergeevna1@gmail.com>
2021-10-15 23:04:51 +0300

    initi commit

---
+++ src/hw2/Interval.java
@@ -0,0 +1,22 @@
+package hw2;
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите целое число:");
+        int x = scan.nextInt();
+
+        if (x>=0 && x <= 14)
+            System.out.println("[0 -14]");
+        if (x>=15 && x <= 35)
+            System.out.println("[15 - 35]");
+        if (x>=36 && x <= 50)
+            System.out.println("[36 - 50]");
+        if (x>=51 && x <= 100)
+            System.out.println("[51 - 100]");
+        if (x<0 || x>100)
+            System.out.println("Введенное число не попадает ни в один из имеющихся промежутков!");
+    }
+}
